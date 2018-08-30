542463fb9bf2070a1cb3f2e1b9f6a780e17498e8
Stepan Trcka <445586@mail.muni.cz>
2018-08-31 01:23:50 +0200

    splay tree

--- AlgorithmsCollection/AlgorithmsCollection/Collections/SplayTree.cs
+++ AlgorithmsCollection/AlgorithmsCollection/Collections/SplayTree.cs
@@ -5,19 +5,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AlgorithmsCollection.Collections
+namespace AlgorithmsCollection
 {
     public class SplayTree<Key, Value> : ICollection<KeyValuePair<Key, Value>>
     {
         private class Node
         {
             public Node Parent { get; set; }
-            public Node Left { get; set; }
-            public Node Right { get; set; }
+            public Node Left { get; set; } = null;
+            public Node Right { get; set; } = null;
             public Key Key { get; set; }
             public Value Value { get; set; }
             public Node GrandParent => Parent?.Parent;
             public bool IsLeftChild => Parent?.Left == this;
+
+            public Node(Key key, Value value, Node parent)
+            {
+                Key = key;
+                Value = value;
+                Parent = parent;
+            }
         }
         
         public struct ReadOnlyNode
@@ -59,6 +66,10 @@ namespace AlgorithmsCollection.Collections
         public ReadOnlyNode? Root => ReadOnlyNode.Create(root);
         public int Count { get; private set; } = 0;
 
+        public SplayTree()
+        {
+        }
+
         public ReadOnlyNode? Find(Key key)
         {
             Node parentNode = null;
@@ -67,7 +78,32 @@ namespace AlgorithmsCollection.Collections
         
         public ReadOnlyNode Insert(Key key, Value value)
         {
-
+            if (root == null)
+            {
+                root = new Node(key, value, null);
+                Count = 1;
+                return ReadOnlyNode.Create(root).Value;
+            }
+            Node parentNode = null;
+            var node = FindImplementation(key, root, ref parentNode);
+            if (node != null) // Key-Value already present, just overwrite current value
+            {
+                node.Value = value;
+                return ReadOnlyNode.Create(node).Value;
+            }
+            node = new Node(key, value, parentNode);
+            var result = KeyComparer.Compare(key, parentNode.Key);
+            if (KeyComparer.Compare(key, parentNode.Key) < 0)
+            {
+                parentNode.Left = node;
+            }
+            else
+            {
+                parentNode.Right = node;
+            }
+            Splay(node);
+            Count++;
+            return ReadOnlyNode.Create(node).Value;
         }
 
         public void Add(KeyValuePair<Key, Value> item) => Insert(item.Key, item.Value);
@@ -147,12 +183,50 @@ namespace AlgorithmsCollection.Collections
                     grandParent.Right = node;
                 }
             }
+            else
+            {
+                root = node;
+            }
             parent.Parent = node;
         }
 
         private void Splay(Node node)
         {
-
+            while (node.Parent != null)
+            {
+                var parent = node.Parent;
+                if (parent.Parent == null) // Zig step
+                {
+                    if (node.IsLeftChild)
+                    {
+                        RightRotation(node);
+                    }
+                    else
+                    {
+                        LeftRotation(node);
+                    }
+                }
+                else if (node.IsLeftChild && parent.IsLeftChild) // Zig-zig step
+                {
+                    RightRotation(parent);
+                    RightRotation(node);
+                }
+                else if (!node.IsLeftChild && !parent.IsLeftChild) // Zig-zig step
+                {
+                    LeftRotation(parent);
+                    RightRotation(node);
+                }
+                else if (node.IsLeftChild && !parent.IsLeftChild) // Zig-zag step
+                {
+                    RightRotation(node);
+                    LeftRotation(node);
+                }
+                else // Zig-zag step
+                {
+                    LeftRotation(node);
+                    RightRotation(node);
+                }
+            }
         }
 
         private Node FindImplementation(Key key, Node node, ref Node resultNodeParent)
---
+++ AlgorithmsCollection/AlgorithmsCollectionUnitTests/SplayTreeTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsCollection;
+
+namespace AlgorithmsCollectionUnitTests
+{
+    [TestClass]
+    public class SplayTreeTests
+    {
+        [TestMethod]
+        public void SplayTreeInsertThreeValues()
+        {
+            var tree = new SplayTree<int, int>();
+            var node1 = tree.Insert(1, 1);
+            var node2 = tree.Insert(2, 2);
+            var node3 = tree.Insert(3, 3);
+            Assert.AreEqual(node1.Key, 1);
+            Assert.AreEqual(node1.Value, 1);
+            Assert.AreEqual(node2.Key, 2);
+            Assert.AreEqual(node2.Value, 2);
+            Assert.AreEqual(node3.Key, 3);
+            Assert.AreEqual(node3.Value, 3);
+            Assert.AreEqual(tree.Count, 3);
+            Assert.IsTrue(tree.Root.HasValue);
+            Assert.AreEqual(tree.Root.Value.Value, 3);
+            Assert.AreEqual(tree.Root.Value.Key, 3);
+            Assert.IsFalse(tree.Root.Value.Parent.HasValue);
+            Assert.IsTrue(tree.Root.Value.Left.HasValue);
+            Assert.IsFalse(tree.Root.Value.Right.HasValue);
+            var left = tree.Root.Value.Left.Value;
+            Assert.AreEqual(left.Value, 2);
+            Assert.AreEqual(left.Key, 2);
+            Assert.IsTrue(left.Parent.HasValue);
+            Assert.AreEqual(left.Parent.Value.Value, 3);
+            Assert.IsFalse(left.Right.HasValue);
+            Assert.IsTrue(left.Left.HasValue);
+            left = left.Left.Value;
+            Assert.AreEqual(left.Value, 1);
+            Assert.AreEqual(left.Key, 1);
+            Assert.IsTrue(left.Parent.HasValue);
+            Assert.AreEqual(left.Parent.Value.Value, 2);
+            Assert.IsFalse(left.Right.HasValue);
+            Assert.IsFalse(left.Left.HasValue);
+        }
+    }
+}
